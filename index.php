8f99aa1e7a5921de63515b27cb619df9dfb497fc
Ben Williams <benwilliams@toumetis.com>
2016-10-31 19:05:13 +0000

    Workshop of Horrors II is a go!

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Workshop of Horrors II</title>
+        <link rel="stylesheet" type="text/css" media="screen" href="css/main.css">
+        <link rel="stylesheet" type="text/css" media="screen" href="css/credits.css">
+        <link rel="shortcut icon" href="favicon.ico">
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-53670757-4', 'auto');
+            ga('send', 'pageview');
+
+        </script>
+    </head>
+    <body>
+        <div class="container" id="container_01">
+            <div class="content" id="content_01">
+                <a class="button" id="tf2" href="http://teamfortress.com/" target="_blank"></a>
+            </div>
+        </div>
+        <div class="container" id="container_02">
+            <div class="content" id="content_02">
+                <div class="text">
+                Every October 31st, with no more room in hell, the costumed children of the damned wander the streets and beckon for offerings of sweets and other goods - and every November 1st, anyone that refused is found face-down in a ditch next to the gas station down the street. This year, they want <span class="italic">more</span> than just candy from those poor souls - and lest they want their bones to be <span class="italic">picked clean</span> by the grubby hands of those apple-bobbing grade-schoolers, the creepy crawly contributors of the Steam Workshop have come together <span class="italic">once more</span> to unleash upon us all a haunting haul of frightening festivities!
+                <br/><br/>
+                Featuring anything from nightmarish nightcaps to horrifying hallowed grounds, the community has pulled through in producing one of the <span class="italic">scariest</span> years of spooky contributions yet! Sure to satisfy the sweet tooth of every candy corn-ravaging devil child, and send shivers down the spines of boils and ghouls alike, the <span class="italic">Workshop of Horrors</span> has opened its doors yet again to the macabre masses!
+                <br/><br/>
+                Mercenary beware, <span class="emph">you</span> <span class="italic">choose the scare!</span>
+                </div>
+            </div>
+        </div>
+        <div class="container" id="container_03">
+            <div class="content" id="content_03">
+            </div>
+        </div>
+        <div class="container" id="container_04">
+            <div class="content" id="content_04">
+                <a class="button" id="workshop" href="//steamcommunity.com/sharedfiles/filedetails/?id=789825470" target="_blank"></a>
+                <a class="button" id="modstf" href="//mods.tf/mods/tag/halloween/" target="_blank"></a>
+                <a class="button" id="server" href="steam://connect/192.223.30.133:27015"></a>
+
+                <div class="text" id="pumpkin">
+                    Thanks to everyone in the community that participated in the Workshop of Horrors this year! While it’s a smaller turn-out this time around, we still wanted to show what the community is capable of doing each and every Halloween, and we couldn’t have done it without any of you! Halloween and Scream Fortress continue to be staples of Team Fortress 2 and we’ve been glad to drive the initiative of the community and showcase their work over the years, and here’s to many more to come!
+                </div>
+
+                <?php include 'php/credits.php';?>
+                <div id="credits">
+                    <div class="row">
+                        <a href="//twitter.com/mellowretro" class="twitter"></a>
+                        <?php steam_block(87084440,"Project Lead, Thumbnail Design, SFM Renders & Web Assets",true);?>
+                        <a href="//twitter.com/aeonbollig" class="twitter"></a>
+                        <?php steam_block(33885066,"Project Lead, Web Assets");?>
+                    </div>
+                    <div class="row">
+                        <div>
+                            <a href="//twitter.com/smashman2004" class="twitter"></a>
+                            <?php steam_block(39933033,"Web Developer");?>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="text" id="thanks">
+                    Special Thanks to Crash, Rogue13, <a href="//tf2maps.net">TF2Maps.net</a>, the <a href="//facepunch.com/showthread.php?t=1520026">TF2 Emporium</a>, <a href="//mods.tf">mods.tf</a>, <a href="//frontline.tf">Team Frontline</a> and everyone at Valve for making this all possible for nine years and counting!
+                </div>
+
+                <a href="http://www.valvesoftware.com/" id="legal" target="_blank"></a>
+            </div>
+        </div>
+    </body>
+</html>
